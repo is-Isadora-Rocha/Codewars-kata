f68d4b12d8a11e0b588c49f74da7a9c155067372
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-05-07 17:41:38 -0300

    número oposto

--- src/oppositeNumber/KataOpposite.java
+++ src/oppositeNumber/KataOpposite.java
@@ -3,5 +3,8 @@ package oppositeNumber;
 public class KataOpposite {
     public static int opposite(int number){
         return -number;
+
+        // return number * -1;
+        // return Math.negateExact(number);
     }
 }
